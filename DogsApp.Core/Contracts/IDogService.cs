f0f6ccffcde98c88d096bd645bf01811ac5824ac
Raya Sergieva <119504714+Razar33@users.noreply.github.com>
2024-12-03 18:08:17 +0200

    Add files via upload

---
+++ DogsApp.Core/Contracts/IDogService.cs
@@ -0,0 +1,14 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+
+namespace DogsApp.Core.Contracts
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, string breed, string picture);
+        bool UpdateDog(int dogId, string name, int age, string breed, string picture);
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+        Dog GetDogById(int dogId);
+        bool RemoveById(int dogId);
+        List<Dog> GetAllDogs(string searchStringBreed, string searchStringName);
+    }
+}
